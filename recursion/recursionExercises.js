0f541a612e7ca9c6df824bb073607cb1b9811acc
Frankie Barreto <barretoacquire@gmail.com>
2021-03-18 14:28:00 -0700

    capitalizedWords, stringifyNumbers

--- recursion/recursionExercises.js
+++ recursion/recursionExercises.js
@@ -135,28 +135,28 @@ const nestedEvenSum = (obj) => {
 }
   
   
-  var obj1 = {
-    outer: 2,
-    obj: {
-      inner: 2,
-      otherObj: {
-        superInner: 2,
-        notANumber: true,
-        alsoNotANumber: "yup"
-      }
-    }
-  }
+//   var obj1 = {
+//     outer: 2,
+//     obj: {
+//       inner: 2,
+//       otherObj: {
+//         superInner: 2,
+//         notANumber: true,
+//         alsoNotANumber: "yup"
+//       }
+//     }
+//   }
   
-  var obj2 = {
-    a: 2,
-    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
-    c: {c: {c: 2}, cc: 'ball', ccc: 5},
-    d: 1,
-    e: {e: {e: 2}, ee: 'car'}
-  };
+//   var obj2 = {
+//     a: 2,
+//     b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//     c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//     d: 1,
+//     e: {e: {e: 2}, ee: 'car'}
+//   };
   
-  console.log(nestedEvenSum(obj1)); // 6
-  console.log(nestedEvenSum(obj2)); // 10
+//   console.log(nestedEvenSum(obj1)); // 6
+//   console.log(nestedEvenSum(obj2)); // 10
 
 
  const capitalizedWords  = (arr) => {
@@ -168,4 +168,35 @@ const nestedEvenSum = (obj) => {
 }
   
   let words = ['i', 'am', 'learning', 'recursion'];
-  console.log(capitalizedWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
+//   console.log(capitalizedWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+const stringifyNumbers = (obj) =>{
+    let result = {};
+
+    for (let key in obj) {
+        let val = obj[key];
+        if(typeof val === 'number') {
+            result[key] = String(val);
+        } else if (typeof val === 'object' && !Array.isArray(val)){
+            result[key] = stringifyNumbers(obj[key]);
+        } else {
+            result[key] = val;
+        }
+    }
+
+    return result;
+}
+
+
+let obj3 = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+console.log(stringifyNumbers(obj3));
