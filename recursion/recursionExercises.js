3a08b9f7d2bdad409b12204e8f5dfc4db291a0f6
Frankie Barreto <barretoacquire@gmail.com>
2021-03-18 12:58:04 -0700

    solved reverse & palindrome

--- recursion/recursionExercises.js
+++ recursion/recursionExercises.js
@@ -43,3 +43,27 @@ function fib(n){
 // fib(10) // 55
 // fib(28) // 317811
 // fib(35) // 9227465
+
+// reverse a string using recursion
+const reverse = (str) => {
+    if(str.length === 1) return str;
+    return reverse(str.slice(1)) + str[0];
+}
+  
+//   console.log(reverse('awesome')) // 'emosewa'
+//   console.log(reverse('rithmschool')) // 'loohcsmhtir'
+
+
+  // check if str is a palindrome
+const isPalindrome = (str) => {
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str[str.length-1]) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+
+// console.log(isPalindrome('awesome')); // false
+// console.log(isPalindrome('foobar')); // false
+// console.log(isPalindrome('tacocat')); // true
+// console.log(isPalindrome('amanaplanacanalpanama')); // true
+// console.log(isPalindrome('amanaplanacanalpandemonium')); // false
