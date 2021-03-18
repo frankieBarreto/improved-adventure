e6e5d9e6971c539910752677654340195d70eb27
Frankie Barreto <barretoacquire@gmail.com>
2021-03-17 17:59:25 -0700

    recursive factorial number

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -4,4 +4,20 @@ const countDown = (n) => {
     countDown(n-1)
 }
 
-countDown(10)
+// countDown(10);
+
+const sumRange = (n) => {
+    if(n === 1) return 1;
+    return sumRange(n-1) + n;
+}
+
+// console.log(sumRange(6));
+
+
+
+const recursiveFactorial = (n) => {
+    if(n === 1) return 1;
+    return n * recursiveFactorial(n-1);
+}
+
+console.log(recursiveFactorial(5))
