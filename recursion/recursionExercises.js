732da389f959ad4c25a53ccefb43d35363e245a5
Frankie Barreto <barretoacquire@gmail.com>
2021-03-18 14:12:46 -0700

    capitalizeedWords, nestedEvenSums

--- recursion/recursionExercises.js
+++ recursion/recursionExercises.js
@@ -108,4 +108,64 @@ const flatten = (arr) => {
 //   console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
 //   console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
 //   console.log(flatten([[1],[2],[3]])) // [1,2,3]
-//   console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
+//   console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+
+const capitalizeFirst = (arr) => {
+    let result = []
+    if(arr.length === 0) return result;
+    result.push(arr[0][0].toUpperCase() + arr[0].slice(1))
+    result = result.concat(capitalizeFirst(arr.slice(1)));
+    return result;
+}
+// console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
+
+
+// Write a recursive function. return the sum of all numbers in the nested obj
+const nestedEvenSum = (obj) => {
+    let sum = 0;
+    for (let key in obj) {
+        if(typeof obj[key] === 'number') {
+            if(obj[key] % 2 === 0) sum += obj[key];
+        } else if (typeof obj[key] === 'object') {
+            sum += nestedEvenSum(obj[key])
+        }
+    }
+
+    return sum;
+}
+  
+  
+  var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+  
+  var obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+  };
+  
+  console.log(nestedEvenSum(obj1)); // 6
+  console.log(nestedEvenSum(obj2)); // 10
+
+
+ const capitalizedWords  = (arr) => {
+     let result = [];
+     if(arr.length === 0) return result;
+     result.push(arr[0].toUpperCase());
+     result = result.concat(capitalizedWords(arr.slice(1)));
+     return result
+}
+  
+  let words = ['i', 'am', 'learning', 'recursion'];
+  console.log(capitalizedWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
