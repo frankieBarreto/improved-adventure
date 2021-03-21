f8c98db113399e19a42d91baab0bfc36f7fb268c
Frankie Barreto <barretoacquire@gmail.com>
2021-03-21 12:15:58 -0700

    naive string search

--- searchAlgorithms/intro.js
+++ searchAlgorithms/intro.js
@@ -7,12 +7,12 @@ findIndex
 */
 
 const linearSearch = (arr, value) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === value) return i;
-    }
-    
-    return -1;
-}
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) return i;
+  }
+
+  return -1;
+};
 //Binary search only works if arr is sorted
 // const linearSearchBigO = (arr, val) => {
 //     let left = 0, right = arr.length-1;
@@ -32,22 +32,44 @@ const linearSearch = (arr, value) => {
 //     return -1;
 // }
 
+// SECTION  O(LogN)
 const binarySearch = (arr, el) => {
-    let start = 0, end = arr.length-1;
-    let mid = Math.floor((start + end) / 2);
+  let start = 0,
+    end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
 
-    while (arr[mid] !== el && start <= end) {
-        if(arr[mid] > el) end = mid - 1;
-        else start = mid + 1;
-        mid = Math.floor((start + end) / 2);
+  while (arr[mid] !== el && start <= end) {
+    if (arr[mid] > el) end = mid - 1;
+    else start = mid + 1;
+    mid = Math.floor((start + end) / 2);
+  }
+
+  return arr[mid] === el ? mid : -1;
+};
+
+// console.log(binarySearch([1,2,3,4,5], 1));
+// console.log(binarySearch([1,2,3,4,5], 2));
+// console.log(binarySearch([1,2,3,4,5], 3));
+// console.log(binarySearch([1,2,3,4,5], 5));
+// console.log(binarySearch([1,2,3,4,5], 6));
+
+const StringSearch = (str, short) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      let c = short[j];
+      let char = str[i+j];
+      // console.log("i:", i, "c:", c, j,"+",i,"=", j+i, "char:", char)
+      if(char !== c) {
+          break;
+      } else if(j === short.length-1) {
+        count++;
+      }
     }
+  }
 
-    return arr[mid] === el ? mid : -1
-}
+  return count;
+};
 
-
-console.log(binarySearch([1,2,3,4,5], 1));
-console.log(binarySearch([1,2,3,4,5], 2));
-console.log(binarySearch([1,2,3,4,5], 3));
-console.log(binarySearch([1,2,3,4,5], 5));
-console.log(binarySearch([1,2,3,4,5], 6));
+console.log(StringSearch("heloworldelo elo", "elo"));
+console.log(StringSearch("lorie loled", "lol"));
