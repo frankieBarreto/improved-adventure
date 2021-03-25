1463c211e17d48bb3194809ed08187b437c3e3bc
Frankie Barreto <barretoacquire@gmail.com>
2021-03-25 11:10:23 -0700

    shift for dbl linked list

--- dataStructures/DoublyLinkedList/doublyLinkedList.js
+++ dataStructures/DoublyLinkedList/doublyLinkedList.js
@@ -19,21 +19,39 @@ class DoublyLinkedList {
         if(!this.head) {
             this.head = node;
             this.tail = node;
-            this.length++;
         } else {
             this.tail.next = node;
+            node.prev = this.tail;
             this.tail = node;
-            this.length++;
         }
+        this.length++;
         return this;
     }
 
     pop() {
-    
+        if(!this.head || !this.tail) return undefined;
+        let popped = this.tail;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = popped.prev;
+            this.tail.next = null;
+            popped.prev = null;
+        }
+        this.length--;
+        return popped;
     }
-
+    
     shift() {
-
+        if(!this.head || !this.tail) return undefined;
+        let shifted = this.head;
+        if(this.length === 1) return this.pop();
+        this.head = shifted.next
+        this.head.prev = null
+        this.length--;
+        shifted.next = null;
+        return shifted;
     }
 
     unshift() {
@@ -55,6 +73,15 @@ class DoublyLinkedList {
     remove() {
 
     }
-
-
 }
+
+const list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+
+console.log(list)
+console.log(`---------------------------------------`)
+console.log(list.shift());
+console.log(`---------------------------------------`)
+console.log(list)
