95b59ad3ee905d5e4ac1e4ba21b9aaf434bad1a1
Frankie Barreto <barretoacquire@gmail.com>
2021-03-26 10:26:47 -0700

    Stack with nodes

--- dataStructures/Stacks/stack.js
+++ dataStructures/Stacks/stack.js
@@ -1,31 +1,79 @@
-class Stack {
-    constructor() {
-        this.store = {};
-        this.size = 0;
-    }
-    push(val) {
-        this.store[this.size] = val;
-        this.size++;
-        return this.size;
-    }
+// class Stack {
+//     constructor() {
+//         this.store = {};
+//         this.size = 0;
+//     }
+//     push(val) {
+//         this.store[this.size] = val;
+//         this.size++;
+//         return this.size;
+//     }
 
-    pop() {
-        let popped = this.store[this.size - 1];
-        if(this.size > 0) {
-            delete this.store[this.size - 1];
-            this.size--;
-        }
+//     pop() {
+//         let popped = this.store[this.size - 1];
+//         if(this.size > 0) {
+//             delete this.store[this.size - 1];
+//             this.size--;
+//         }
 
-        return popped;
-    }
+//         return popped;
+//     }
     
-    length() {
-        return this.size;
+//     length() {
+//         return this.size;
+//     }
+// }
+
+
+
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
     }
 }
 
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    
+    push(val) {
+        let node = new Node(val);
+        if(!this.first) {
+            this.first = node;
+            this.last = node;
+        } else {
+            let temp = this.first;
+            this.first = node;
+            this.first.next = temp;
+        }
+
+        return ++this.size;
+    }
+
+    pop(){
+        if(!this.first) return null;
+        let popped = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+            this.size--;
+        }
+        return popped.val;
+    }
+
+    size() {
+        return this.size;
+    }
+    
+}
 let stack = new Stack();
-console.log(stack.push("hello"));
+stack.push("hello");
 stack.push("world");
-stack.pop();
-console.log(stack);
+stack.push("stack");
+
+console.log(stack.pop(), stack);
