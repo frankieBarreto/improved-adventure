ec64a9d15c6f41f95f9adcb9f436eca3cce21ae8
Frankie Barreto <barretoacquire@gmail.com>
2021-04-20 11:38:58 -0700

    min, max , reversed

---
+++ Python3/min_max.py
@@ -0,0 +1,34 @@
+names = ['Arya', "Samson", "Dora", "Tim", "Ollivander"]
+
+# finds the minimum length of a name in names
+min(len(name) for name in names) # 3
+
+# find the longest name itself
+max(names, key=lambda n:len(n)) #Ollivander
+
+songs = [
+	{"title": "happy birthday", "playcount": 1},
+	{"title": "Survive", "playcount": 6},
+	{"title": "YMCA", "playcount": 99},
+	{"title": "Toxic", "playcount": 31}
+]
+
+# Finds the song with the lowerest playcount
+min(songs, key=lambda s: s['playcount']) #{"title": "happy birthday", "playcount": 1}
+
+# Finds the title of the most played song
+max(songs, key=lambda s: s['playcount'])['title'] #YMCA
+
+l1 = [1,2,3,4,5]
+t1 = (99, 25,30,-7)
+string = "alcatraz"
+def extremes(data):
+	# mi = min([el for el in data])
+	# ma = max([el for el in data])
+	# return(tuple([mi, ma]))
+	return (min(data), max(data))
+
+print(extremes(string))
+
+
+
---
+++ Python3/reverse.py
@@ -0,0 +1,13 @@
+
+rev_str = reversed("hello world")
+print(rev_str) # reversed object 
+rev_str = list(rev_str) # turned into a list
+print(rev_str)
+rev_str = "".join(rev_str) # join it to a string
+print(rev_str) # final string revered or just use slice
+
+
+for n in reversed(range(0, 11)):
+    if n == 0: print('blast off!')
+    else: 
+        print(n)
