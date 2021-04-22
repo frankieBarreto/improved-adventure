da449f21ed95c04a93ee7cab40b58373695aa240
Frankie Barreto <barretoacquire@gmail.com>
2021-04-22 12:06:02 -0700

    class methods, chicken class with class attributes

--- Python3/OOP/py_oop.py
+++ Python3/OOP/py_oop.py
@@ -20,3 +20,20 @@ class BankAccount:
             return "Amount requested exceeds balance."
         return self.balance
 
+
+
+class Chicken:
+    total_eggs = 0
+    def __init__(self, name, species, eggs=0):
+        self.name = name
+        self.species = species
+        self.eggs = eggs
+    
+    def lay_egg(self):
+        self.eggs += 1
+        Chicken.total_eggs += 1
+        return self.eggs
+    
+
+
+
