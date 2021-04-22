9213dee02c584332ec3aefce0dfe7f223223c4a7
Frankie Barreto <barretoacquire@gmail.com>
2021-04-22 11:57:09 -0700

    created bank account class, class attributes

--- Python3/OOP/py_oop.py
+++ Python3/OOP/py_oop.py
@@ -3,3 +3,20 @@ class Comment:
         self.username = username
         self.text = text
         self.likes = likes
+
+
+class BankAccount:
+    def __init__(self, owner):
+        self.owner = owner
+        self.balance = 0.0
+    
+    def deposit(self,amount):
+        self.balance += amount
+        return self.balance
+    
+    def withdraw(self, amount):
+        if self.balance >= amount: self.balance -= amount
+        else:
+            return "Amount requested exceeds balance."
+        return self.balance
+
