659caa05f23a3e06888214dc7f802ef32cd7abc7
Frankie Barreto <barretoacquire@gmail.com>
2021-04-22 17:28:34 -0700

    class inheritance, super __init__

---
+++ Python3/OOP/simple_inheritance.py
@@ -0,0 +1,38 @@
+class Animal:
+	cool = True
+
+	def __init__(self, name, species):
+		self.name = name
+		self.species = species
+
+	def make_sound(self, sound):
+		print(f"this animal says {sound}")
+
+	def __repr__(self):
+		return f"{self.name} is a {self.species}."
+
+
+# Cat class inherits from Animal
+class Cat(Animal):
+	def __init__(self, name, species, breed, toy):
+		super().__init__(self, name, species)
+		self.breed = breed
+		self.toy = toy
+
+
+
+# Make a new cat instance
+blue = Cat("Blue", "Cat", "Scottish Fold", "fishing-pole")
+
+# Because of inheritance, a Cat has access to:
+print(blue)
+blue.make_sound("Meow")
+
+print(blue.cool)
+print(blue.species)
+print(blue.toy)
+
+#blue is both a Cat and Animal (and base object)
+print(isinstance(blue, Cat))
+print(isinstance(blue, Animal))
+print(isinstance(blue, object))
