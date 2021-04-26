d242866276834e3c06fab52312435d9220cee533
Frankie Barreto <barretoacquire@gmail.com>
2021-04-26 13:20:05 -0700

    test for deck amd card completed

--- Python3/Deck/deck_tests.py
+++ Python3/Deck/deck_tests.py
@@ -12,8 +12,67 @@ class CardTests(unittest.TestCase):
         self.assertEqual(self.card.value, "A")
 
     def test_repr(self):
-        """repr should return a string of the form eck"""
-        self.assertEqual(repr(self.card.value, "A of Hearts"))
+        """repr should return a string of the form deck"""
+        self.assertEqual(repr(self.card), "A of Hearts")
+
+class DeckTests(unittest.TestCase):
+    def setUp(self):
+        self.deck = Deck()
+
+    def test_init(self):
+        """decks should return a cards attribute, which is a list with 52 elements"""
+        self.assertTrue(isinstance(self.deck.cards, list))
+        self.assertEqual(len(self.deck.cards), 52)
+    
+    def test_repr(self):
+        """repr should return a string of the form "Deck of 52 cards"""
+        self.assertEqual(repr(self.deck),"Deck of 52 cards")
+
+    def test_count(self):
+        self.assertEqual(self.deck.count(), 52)
+        self.deck.cards.pop()
+        self.assertEqual(self.deck.count(), 51)
+
+    def test_deal_sufficient_cards(self):
+        """_deal should deal the number of cards specified"""
+        cards = self.deck._deal(10)
+        self.assertEqual(len(cards), 10)
+        self.assertEqual(self.deck.count(), 42)
+
+    def test_deal_insufficient_cards(self):
+        """_deal should deal the number of cards left in the deck"""
+        cards = self.deck._deal(100)
+        self.assertEqual(len(cards), 52)
+        self.assertEqual(self.deck.count(), 0)
+
+    def test_deal_no_cards(self):
+        """_deal should throw a ValueError if deck is empty"""
+        cards = self.deck._deal(self.deck.count())
+        with self.assertRaises(ValueError):
+            self.deck._deal(1)
+
+    def test_deal_hand(self):
+        """deal_hand should deal the number of cards passed into it"""
+        cards = self.deck.deal_hand(20)
+        self.assertEqual(len(cards), 20)
+        self.assertEqual(self.deck.count(), 32)
+    
+    def test_shuffle_full_deck(self):
+        """shuffle should shuffle deck if deck is full"""
+        cards = self.deck.cards[:]
+        self.deck.shuffle()
+        self.assertNotEqual(cards, self.deck.cards)
+        self.assertEqual(self.deck.count(), 52)
+
+    def test_shuffle_not_full_deck(self):
+        self.deck._deal(1)
+        with self.assertRaises(ValueError):
+            self.deck.shuffle()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
 
 
 
