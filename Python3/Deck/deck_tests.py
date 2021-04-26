f93c4f020a899cdcfbc8cb0ee3ac9a9273ccf3f2
Frankie Barreto <barretoacquire@gmail.com>
2021-04-26 12:36:11 -0700

    CardTest class created for teting cards

---
+++ Python3/Deck/deck_tests.py
@@ -0,0 +1,17 @@
+from card import Card
+from deck import Deck
+import unittest
+
+class CardTests(unittest.TestCase):
+    def setUp(self):
+        self.card = Card("Hearts", "A")
+
+    def test_init(self):
+        """cards should have a suit and value"""
+        self.assertEqual(self.card.suit, "Hearts")
+        self.assertEqual(self.card.value, "A")
+
+    def test_repr(self):
+        """repr should return a string of the form eck"""
+        self.assertEqual(repr(self.card.value, "A of Hearts"))
+
