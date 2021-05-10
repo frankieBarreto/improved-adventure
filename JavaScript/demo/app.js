c470e753e00808a96c4b97e80722af7b1a2a5156
Frankie Barreto <barretoacquire@gmail.com>
2021-05-10 15:31:19 -0700

    js

---
+++ JavaScript/demo/app.js
@@ -0,0 +1,8 @@
+// Put your code in a JS file
+alert("Hello from Demo") // will show
+
+// won't show up
+'hi'.toUpperCase()
+
+// shows up
+console.log('hi'.toUpperCase())
---
+++ JavaScript/demo/app2.js
@@ -0,0 +1,2 @@
+console.error('Yeeeerrrr');
+console.log(3+4)
